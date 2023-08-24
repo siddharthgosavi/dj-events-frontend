c40e986837978832fe986000683e2939bb7d6719
Siddharth <siddhugosavi89@gmail.com>
2023-08-24 20:34:52 +0530

    404 page added

---
+++ pages/404.js
@@ -0,0 +1,23 @@
+import { FaExclamationTriangle, FaSadTear } from "react-icons/fa";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import styles from "../styles/404.module.css";
+
+export default function NotFoundPage() {
+  return (
+    <Layout title="Page not Found">
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle />
+          404
+        </h1>
+        <h4>
+          <FaSadTear />
+          {"\t"}
+          Sorry, Page not found!
+        </h4>
+        <Link href={"/"}>Go back to Home</Link>
+      </div>
+    </Layout>
+  );
+}
