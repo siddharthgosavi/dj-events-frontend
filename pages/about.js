a1f32deb99493d8b7699a2e906216ae703eb1376
Siddharth <siddhugosavi89@gmail.com>
2023-08-24 19:29:06 +0530

    Next js Starter

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an app to find latest DJ and other Events</p>
+      <span>Version: 1.0.0</span>
+    </div>
+  );
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,3 @@
+export default function SingleEventPage() {
+  return <h1>SingleEventPage</h1>;
+}
